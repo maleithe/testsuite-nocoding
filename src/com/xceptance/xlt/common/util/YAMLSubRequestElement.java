e2d8791cd86d8d20a34d02cb210bab453781b438
maleithe <m.aleithe@xceptance.net>
2015-02-16 14:01:17 +0100

    read XHR as subrequest

--- src/com/xceptance/xlt/common/util/YAMLSubRequestElement.java
+++ src/com/xceptance/xlt/common/util/YAMLSubRequestElement.java
@@ -7,9 +7,13 @@ public class YAMLSubRequestElement
 {
     public static final String STATIC = "Static";
 
+    public static final String XHR = "XHR";
+
     // TODO create an object for XHR
 
     private final ArrayList<String> staticUrl;
+
+    private final Map<String, Object> xhrUrl;
 
     /**
      * encapsulate the YAML SubRequestElement
@@ -19,20 +23,50 @@ public class YAMLSubRequestElement
      */
     public YAMLSubRequestElement(final Object yamlSubRequestElement)
     {
-        final ArrayList<String> _yamlSubRequestElement = (ArrayList) ((Map) yamlSubRequestElement).get(STATIC);
+        final Map<?, ?> _yamlSubRequestElementType = (Map) yamlSubRequestElement;
+        final ArrayList<String> _yamlSubRequestElementStatic;
+
+        boolean isStatic = false;
+        boolean isXHR = false;
+
+        if (_yamlSubRequestElementType.containsKey(STATIC))
+        {
+            System.out.println("ist static");
+            _yamlSubRequestElementStatic = (ArrayList) ((Map) yamlSubRequestElement).get(STATIC);
+            this.xhrUrl = null;
+            isStatic = true;
+        }
+        else if (_yamlSubRequestElementType.containsKey(XHR))
+        {
+            System.out.println("ist XHR");
+            this.xhrUrl = (Map) ((Map) yamlSubRequestElement).get(XHR);
+            _yamlSubRequestElementStatic = null;
+            isXHR = true;
+        }
+        else
+        {
+            System.out.println("ist null");
+            _yamlSubRequestElementStatic = null;
+            this.xhrUrl = null;
+        }
 
         this.staticUrl = new ArrayList<String>();
-        for (final String url : _yamlSubRequestElement)
+
+        // write static URLs to constructor
+        if (isStatic)
         {
-            this.staticUrl.add(url);
+            for (final String url : _yamlSubRequestElementStatic)
+            {
+                this.staticUrl.add(url);
+            }
         }
     }
-    
+
     public ArrayList<String> getYAMLSubRequestElement()
     {
         return this.staticUrl;
     }
-    
+
     public boolean isStaticContentURL()
     {
         return !this.staticUrl.isEmpty();
